3b790e6a627e2c4554df93f3eadd56d0339f4d01
Razbichaha <91633207+Razbichaha@users.noreply.github.com>
2022-09-08 20:54:36 +0300

    AddSound
    Изменил скрипт включения аларма

--- Assets/Scripts/AlarmController.cs
+++ Assets/Scripts/AlarmController.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class AlarmController : MonoBehaviour
@@ -10,12 +10,16 @@ public class AlarmController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         _animator.SetBool("isAlarm", true);
-        _audioSource.mute = false;
+        _audioSource.Play();
+        _audioSource.loop = true;
+       // _audioSource.mute = false;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _animator.SetBool("isAlarm", false);
-        _audioSource.mute = true;
+        _audioSource.loop = false;
+       // _audioSource.Stop();
+       // _audioSource.mute = true;
     }
 }
